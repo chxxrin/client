0ca50ab632f94e266c9bbe29b616f9d890c4c211
wooseok123 <hosmimam@naver.com>
2023-07-07 19:37:13 +0900

    feat : default setting

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import GlobalStyle from './@style/GlobalStyle'
+import './@style/font.css'
+
+function App() {
+  return (
+    <>
+      <GlobalStyle />
+      안녕
+    </>
+  )
+}
+
+export default App
