848b59a1e6f963d614be942901f05cb4c548deb5
Goonco <s07019@naver.com>
2023-07-19 14:30:32 +0900

    :lipstick: 검색박스 스타일링, MyButton.js 생성 (#2)

--- src/@pages/social/SearchBox.js
+++ src/@pages/social/SearchBox.js
@@ -1,21 +1,64 @@
 import { styled } from 'styled-components';
-import Button from 'src/@components/atoms/Button';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
+import { colors } from './utils/palette';
+
+// Imported Components
+import MyButton from './MyButton';
+import { Text } from 'src/@components/atoms/Text';
 
 const SearchBox = () => {
   return (
     <SearchBoxContainer>
-      <input />
-      <Button paddingX="10" paddingY="5">
-        검색
-      </Button>
+      <FontAwesomeIcon
+        className="fa-2xl"
+        style={{ color: colors.themeGray }}
+        icon={faMagnifyingGlass}
+      />
+      <SocialInput placeholder="스터디명 또는 사용자명을 입력해 주세요." />
+      <MyButton width="70" height="40" color={colors.themeRed}>
+        <Text size="20" weight="bold" color="#fff">
+          검색
+        </Text>
+      </MyButton>
     </SearchBoxContainer>
   );
 };
 
 const SearchBoxContainer = styled.div`
+  box-sizing: border-box;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+
   margin: 0 auto;
+  border: 1px solid #babcbe;
+  padding-left: 20px;
+  padding-right: 20px;
   width: 570px;
   height: 70px;
+  border-radius: 10px;
+
+  background-color: ${colors.themeBG};
+`;
+
+const SocialInput = styled.input`
+  border: none;
+  width: 390px;
+  height: 50px;
+
+  text-indent: 10px;
+  font-size: 20px;
+  line-height: 20px;
+
+  outline: none;
+
+  &::placeholder {
+    font-size: 20px;
+    line-height: 20px;
+    font-weight: 600;
+    color: ${colors.themeGray};
+  }
 `;
 
 export default SearchBox;
