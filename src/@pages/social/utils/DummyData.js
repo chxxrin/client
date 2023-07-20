20634c31fce5911dcbc0758665903be00113ee08
Goonco <s07019@naver.com>
2023-07-20 15:33:40 +0900

    :art: dummy data 및 finder.css 수정 (#2)

---
+++ src/@pages/social/utils/DummyData.js
@@ -0,0 +1,11 @@
+export const RankingBoxStudyData = [
+  {
+    name: '알고리즘 씹어먹기',
+    rank: '5',
+    lv: '✈ 성층권',
+    solved: '23',
+    mvp: '윤태호',
+    member: [],
+    published: true,
+  },
+];
