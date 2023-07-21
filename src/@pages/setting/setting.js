e0684d9393e1e8f3c7ff3c42c6ec5a83a0a283d0
Sehwan-Chang <kdwkdwkd@naver.com>
2023-07-21 21:37:03 +0900

    ✨ 프로필관리 세부정보 및 정보 수정 버튼 추가(#3)

--- src/@pages/setting/setting.js
+++ src/@pages/setting/setting.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import styled from 'styled-components';
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faUser, faBuilding } from '@fortawesome/free-solid-svg-icons';
+import { faGithub } from '@fortawesome/free-brands-svg-icons';
 const studies = ['스터디 1', '스터디 2', '스터디 3', '스터디 4'];
 
 const Setting = () => {
@@ -31,10 +33,60 @@ const StudyBox = ({ study }) => {
   // study prop 이용해서 각 스터디 박스 표현
   return <Box>{study} 관리 UI</Box>;
 };
-
+const ProfileItem = ({ title, content, paddingLeft, fontColor }) => {
+  return (
+    <ProfileItemStyled paddingLeft={paddingLeft} fontColor={fontColor}>
+      <ProfileTitle>{title}</ProfileTitle>
+      <ProfileContent>{content}</ProfileContent>
+    </ProfileItemStyled>
+  );
+};
 const ProfileBox = () => {
   // 프로필 관리 UI
-  return <Box>프로필 관리 UI</Box>;
+  return (
+    <ProfileItemWrapper>
+      <Box>
+        <Row>
+          <ProfileItem title="사진" content="" paddingLeft="10" />
+          <ProfileItem title="3위" content="장세환" paddingLeft="20" />
+          <ProfileItem title="팔로우" content="129명" paddingLeft="50" />
+          <ProfileItem title="팔로잉" content="32명" paddingLeft="50" />
+          <ProfileItem
+            title="푼 문제"
+            content="18문제"
+            paddingLeft="50"
+            fontColor="#F05454"
+          />
+        </Row>
+        <Row />
+        <Row />
+        <Row />
+        <Row>
+          <IconProfileContainer />
+          <Button>내 정보 수정</Button>
+        </Row>
+      </Box>
+    </ProfileItemWrapper>
+  );
+};
+
+const IconProfileItem = ({ icon, text }) => {
+  return (
+    <IconProfileItemStyled>
+      <FontAwesomeIcon icon={icon} size="1x" />
+      <IconProfileText>{text}</IconProfileText>
+    </IconProfileItemStyled>
+  );
+};
+
+const IconProfileContainer = () => {
+  return (
+    <IconProfileContainerStyled>
+      <IconProfileItem icon={faUser} text="장세환" />
+      <IconProfileItem icon={faGithub} text="@sehwanchang" />
+      <IconProfileItem icon={faBuilding} text="서강대학교 | 멋쟁이사자처럼" />
+    </IconProfileContainerStyled>
+  );
 };
 
 const Sidebar = styled.div`
@@ -80,14 +132,24 @@ const SettingTitle = styled.h1`
 const Box = styled.div`
   width: 495px;
   height: 195px;
-  background: grey;
+  background: white;
   margin-top: 20px;
   display: flex;
-  align-items: center;
-  justify-content: center;
+  flex-direction: column;
+  align-items: flex-start;
+  justify-content: flex-start;
   color: white;
+  border-radius: 10px 10px 10px 10px;
+  box-shadow: 0.5px 0.5px 1.5px 1.5px gray;
 `;
-
+const Row = styled.div`
+  display: flex;
+  flex-direction: row; // 가로 방향으로 조정
+  align-items: center;
+  justify-content: flex-start;
+  margin-top: 10px;
+  margin-left: 5px;
+`;
 const StudyWrapper = styled.div`
   display: flex;
   flex-direction: column;
@@ -102,4 +164,52 @@ const ProfileWrapper = styled.div`
   width: 50%;
 `;
 
+const ProfileItemWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+const ProfileTitle = styled.div`
+  font-size: 15px;
+  font-weight: 500;
+  color: black;
+`;
+const ProfileContent = styled.div`
+  font-size: 18px;
+  font-weight: 700;
+  color: inherit;
+`;
+
+const ProfileItemStyled = styled.div`
+  padding-top: 20px;
+  padding-left: ${(props) => props.paddingLeft}px;
+  width: 55px;
+  height: 51px;
+  color: ${(props) => props.fontColor || 'black'};
+`;
+
+const IconProfileContainerStyled = styled.div`
+width : 162px
+height : 75px
+display : flex
+flex-direction : column;
+justify-content : space-around;
+`;
+
+const IconProfileItemStyled = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+const IconProfileText = styled.div`
+  padding-left: 10px;
+  color: grey;
+`;
+
+const Button = styled.button`
+  width: 255px;
+  height: 33px;
+  font-size: 15px;
+  font-weight: 500;
+`;
 export default Setting;
