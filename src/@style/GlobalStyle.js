c166fd38f2b4c1b32ba87737c21edd1544dd316b
wooseok123 <hosmimam@naver.com>
2023-07-26 21:13:52 +0900

    :lipstick: 전역 스타일링 (#7)

--- src/@style/GlobalStyle.js
+++ src/@style/GlobalStyle.js
@@ -1,11 +1,14 @@
-import { createGlobalStyle } from 'styled-components'
-import reset from 'styled-reset'
+import { createGlobalStyle } from 'styled-components';
+import reset from 'styled-reset';
 
 const GlobalStyle = createGlobalStyle`
     ${reset};
     body {
         font-family : "Pretendard-Regular";
     }
-`
+    body * {
+        box-sizing : border-box;
+    }
+`;
 
-export default GlobalStyle
+export default GlobalStyle;
