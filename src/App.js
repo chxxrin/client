45eac53aca08ab55de99224e03099027c7165421
wooseok123 <hosmimam@naver.com>
2023-08-09 15:49:41 +0900

    :lipstick: react-query 설치

--- src/App.js
+++ src/App.js
@@ -3,14 +3,27 @@ import GlobalStyle from './@style/GlobalStyle';
 import './@style/font.css';
 import AppContainer from './@components/templates/AppContainer';
 import { BrowserRouter } from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 function App() {
+  const context = {
+    defaultOptions: {
+      queries: {
+        staleTime: 1000 * 60,
+        cacheTime: 1000 * 60 * 5,
+      },
+    },
+  };
+  const queryClient = new QueryClient(context);
+
   return (
     <BrowserRouter>
-      <GlobalStyle />
-      <AppContainer>
-        <Router />
-      </AppContainer>
+      <QueryClientProvider client={queryClient}>
+        <GlobalStyle />
+        <AppContainer>
+          <Router />
+        </AppContainer>
+      </QueryClientProvider>
     </BrowserRouter>
   );
 }
