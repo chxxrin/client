0d64aa05ef535004999c429185d6dbbfa7721e98
wooseok123 <hosmimam@naver.com>
2023-08-10 20:47:43 +0900

    :sparkles: add routing logic (#10)

--- src/Router.js
+++ src/Router.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Routes, Route, BrowserRouter, useLocation } from 'react-router-dom';
+import { Routes, Route, Redirect, useLocation } from 'react-router-dom';
 import Home from './@pages/home/home';
 import SignUp from './@pages/signUp/signUp';
 import Exam from './@pages/exam/exam';
@@ -7,24 +7,34 @@ import Social from './@pages/social/social';
 import Setting from './@pages/setting/setting';
 import Aside from './@components/organisms/AsideBar';
 import Kakao from '@pages/Kakao';
+import { useParams } from 'react-router-dom/dist';
 
 const Router = () => {
   const { pathname } = useLocation();
-
-  const excludeAside = ['/', '/kakao', '/signUp'];
+  const { spaceId } = useParams(); // URL에서 spaceId를 추출
+  const excludeAside = ['/', '/kakao', '/signUp', '/home'];
 
   return (
     <>
       {excludeAside.includes(pathname) ? '' : <Aside />}
       <Routes>
+        <Route path="/:spaceId/*" element={<SpaceRoutes />} />
         <Route path="/" element={<Home />} />
-        <Route path="/kakao" element={<Kakao />} />
-        <Route path="/signUp" element={<SignUp />} />
-        <Route path="/exam" element={<Exam />} />
-        <Route path="/social" element={<Social />} />
-        <Route path="/setting" element={<Setting />} />
       </Routes>
     </>
+  );
+};
+
+const SpaceRoutes = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/kakao" element={<Kakao />} />
+      <Route path="/signUp" element={<SignUp />} />
+      <Route path="/exam" element={<Exam />} />
+      <Route path="/social" element={<Social />} />
+      <Route path="/setting" element={<Setting />} />
+    </Routes>
   );
 };
 
