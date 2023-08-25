4cda68f32788750d08d1ece1076547d91625ce37
wooseok123 <hosmimam@naver.com>
2023-08-25 13:42:56 +0900

    :sparkles: 로그인 관련 라우팅 로직 개선

--- src/Router.js
+++ src/Router.js
@@ -1,5 +1,11 @@
-import React from 'react';
-import { Routes, Route, Redirect, useLocation } from 'react-router-dom';
+import React, { useEffect } from 'react';
+import {
+  Routes,
+  Route,
+  Redirect,
+  useLocation,
+  useNavigate,
+} from 'react-router-dom';
 import Home from './@pages/home/home';
 import SignUp from './@pages/signUp/signUp';
 import Exam from './@pages/exam/exam';
@@ -7,19 +13,45 @@ import Social from './@pages/social/social';
 import Setting from './@pages/setting/setting';
 import Aside from './@components/organisms/AsideBar';
 import Kakao from '@pages/Kakao';
-import { useParams } from 'react-router-dom/dist';
+import { getCookie } from '@utils/util';
+import solveItService from './@service/solveItService';
+import axios from 'axios';
 
 const Router = () => {
   const { pathname } = useLocation();
-  const { spaceId } = useParams(); // URL에서 spaceId를 추출
   const excludeAside = ['/', '/kakao', '/signUp', '/home'];
+
+  const navigate = useNavigate();
+  const getAccessToken = async (refreshToken) => {
+    const data = await solveItService.getAccessToken(refreshToken);
+    return data;
+  };
+
+  useEffect(() => {
+    const refreshToken = getCookie('token');
+    const data = {
+      refresh: refreshToken,
+    };
+
+    if (!refreshToken) navigate('/');
+
+    // try {
+    //   const { access } = getAccessToken(data);
+    //   axios.defaults.headers.common['Authorization'] = access;
+    // } catch (err) {
+    //   console.err(err);
+    //   navigate('/');
+    // }
+  }, []);
 
   return (
     <>
       {excludeAside.includes(pathname) ? '' : <Aside />}
       <Routes>
-        <Route path="/:spaceId/*" element={<SpaceRoutes />} />
+        <Route path="/kakao" element={<Kakao />} />
+        <Route path="/signUp" element={<SignUp />} />
         <Route path="/" element={<Home />} />
+        <Route path="/:spaceId/*" element={<SpaceRoutes />} />
       </Routes>
     </>
   );
@@ -29,7 +61,6 @@ const SpaceRoutes = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
-      <Route path="/kakao" element={<Kakao />} />
       <Route path="/signUp" element={<SignUp />} />
       <Route path="/exam" element={<Exam />} />
       <Route path="/social" element={<Social />} />
