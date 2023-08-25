40598e368a7cb8bcfd968aa6fb6671d71942a52e
wooseok123 <hosmimam@naver.com>
2023-08-25 13:49:12 +0900

    :sparkles: 카카오 로그인 페이지 개발

--- src/@pages/Kakao/index.js
+++ src/@pages/Kakao/index.js
@@ -1,25 +1,30 @@
-import { useQuery } from '@tanstack/react-query';
 import { setCookie } from '@utils/util';
-import axios from 'axios';
 import React, { useEffect } from 'react';
-import { Service } from 'src/@service/kakaoService';
+import KakaoService from 'src/@service/kakaoService';
 import { useNavigate } from 'react-router-dom/dist';
 
 const Index = () => {
   const navigate = useNavigate();
 
   const code = new URL(document.location.toString()).searchParams.get('code');
+
   const fetchToken = async () => {
     try {
-      const { data } = await Service.getToken(code);
+      const { data } = await KakaoService.getToken(code);
       setCookie('token', data.refresh);
-      if (!data.created) navigate('/');
+
+      if (data.created) {
+        navigate('/0/home');
+      } else {
+        navigate('/signUp');
+      }
     } catch (err) {
       console.error(err);
     }
   };
   useEffect(() => {
     if (code) {
+      console.log(code);
       fetchToken();
     }
   }, [code]);
